658398b10840efef7f76fe08b7e646622592fdb5
adir-ch <actiger@gmail.com>
2017-02-22 17:02:26 +1100

    Forgot to add the Deductions interface

---
+++ PayCalculator/PayCalculator/PayCalculator.core.Model/Salary/IDeductions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PayCalculator.core.Model.Salary
+{
+    public interface IDeductions
+    {
+        decimal GetTotalDeductionsAmount(decimal taxableIncome);
+        IList<Tuple<string, decimal>> GetDeductionsReport(); 
+    }
+}
