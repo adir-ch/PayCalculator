15e4c20c1078cc550f91f8630f22a3331061ca7e
adir-ch <actiger@gmail.com>
2017-02-22 18:30:09 +1100

    Starting employee tests

---
+++ PayCalculator/PayCalculator.core.BusinessObjects.Test/Employee/EmployeeShould.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.core.BusinessObjects.Test.Employee
+{
+    [TestFixture]
+    public class EmployeeShould
+    {
+        [Test]
+        public void InitEmployeeObject()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
