98bb624f2d22af17414bf6ddf4a18f4934372a6e
adir-ch <actiger@gmail.com>
2017-02-22 12:16:30 +1100

    Added some fake infra structure (not implemented yet)

---
+++ PayCalculator/PayCalculator.Infra.DataAccess/MemoryDB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.Infra.DataAccess
+{
+    public class MemoryDB
+    {
+    }
+}
---
+++ PayCalculator/PayCalculator/PayCalculator.Infra.WebApi/PayCalculatorWebApi.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace PayCalculator.Infra.WebApi
+{
+    public class PayCalculatorWebApi
+    {
+        private static readonly Lazy<PayCalculatorWebApi> _instance = new Lazy<PayCalculatorWebApi>(() => new PayCalculatorWebApi());
+        private readonly Injector _injector;
+
+        public static PayCalculatorWebApi Instance
+        {
+            get { return _instance.Value; }
+        }
+
+        private PayCalculatorWebApi()
+        {
+            _injector = IoC.Injector.Instance;
+        }
+
+        public IServiceResponse CallService(IServiceRequest request)
+        {
+            IBusinessService service;
+            switch (request.ServiceName)
+            {
+                case "EmployeeAddService":
+                    {
+                        service = _injector.Inject<IBusinessService>("EmployeeAddService");
+                        break;
+                    }
+
+                case "EmployeeCalculateSalaryService":
+                    {
+                        service = _injector.Inject<IBusinessService>("EmployeeCalculateSalaryService");
+                        break;
+                    }
+
+                case "EmployeeLookupService":
+                    {
+                        service = _injector.Inject<IBusinessService>("EmployeeLookupService");
+                        break;
+                    }
+
+                default:
+                    throw new NotImplementedException();
+            }
+
+            return service.Execute(request);
+        }
+    }
+}
