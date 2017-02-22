0c7f900c5057956d6a5ed67640b9475bff5fb6e6
adir-ch <actiger@gmail.com>
2017-02-22 12:06:00 +1100

    Coding a pay calculator client

--- PayCalculator/PayCalculator/Client.cs
+++ PayCalculator/PayCalculator/Client.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,33 @@ namespace PayCalculator
 {
     class Client
     {
+        // this is actually a poor man way to mimic a REST API request
+        private PayCalculatorWebApi _webApi = PayCalculatorWebApi.Instance;
+        private readonly ILog _log = LogManager.GetLogger("ClientLogger");
+
+        public Client()
+        {
+            _log.Debug("Starting Pay calculator client");
+        }
+
+        public void AddNewEmployee(string employeeName, string location, string grossSalary)
+        {
+            _log.DebugFormat("Adding new employee: {0}", employeeName); 
+        }
+
+        public void CalculateEmployeeNetSalary(string employeeName)
+        {
+            _log.DebugFormat("Calculating employee net annual salary: {0}", employeeName); 
+            
+        }
+
+        public string PrintEmployeeSalaryReport(string employeeName)
+        {
+            _log.DebugFormat("Generating salary report for employee: {0}", employeeName); 
+            return String.Empty; 
+        }
+
+        
         static void Main(string[] args)
         {
             string employeeName;
@@ -15,7 +43,9 @@ namespace PayCalculator
             ReadDataFromConsole(out employeeName, out location, out grossSalary);
 
             Client client = new Client();
-            string employeeReport = "working on it"; 
+            client.AddNewEmployee(employeeName, location, grossSalary);
+            client.CalculateEmployeeNetSalary(employeeName); // going lazy 
+            var employeeReport = client.PrintEmployeeSalaryReport(employeeName);
 
             Console.WriteLine("---------------------");
             Console.WriteLine(employeeReport);
@@ -24,14 +54,14 @@ namespace PayCalculator
 
         private static void ReadDataFromConsole(out string employeeName, out string location, out string grossSalary)
         {
-            //Console.Write("Employee name: ");
-            employeeName = "ADIR";  //Console.ReadLine();
+            Console.Write("Employee name: ");
+            employeeName = Console.ReadLine();
 
-            //Console.Write("Employee location: ");
-            location = "Australia";  //Console.ReadLine();
+            Console.Write("Employee location: ");
+            location = Console.ReadLine();
 
-            //Console.Write("Gross Salary: ");
-            grossSalary = "200,000"; // Console.ReadLine();
+            Console.Write("Gross Salary: ");
+            grossSalary = Console.ReadLine();
         }
     }
 }
