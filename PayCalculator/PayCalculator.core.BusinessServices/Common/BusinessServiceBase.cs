0d74f10dc385ecb219afc51bbb0442c807b3b1c6
adir-ch <actiger@gmail.com>
2017-02-22 23:57:24 +1100

    Added Service base class + employee add service (not testing services)

---
+++ PayCalculator/PayCalculator.core.BusinessServices/Common/BusinessServiceBase.cs
@@ -0,0 +1,63 @@
+﻿using log4net;
+using PayCalculator.Contracts.Common;
+using PayCalculator.core.Model.Common;
+using System;
+
+namespace PayCalculator.core.BusinessServices.Common
+{
+    public abstract class BusinessServiceBase<TRequest, TResponse> : IBusinessService
+        where TRequest : ServiceRequestBase, new()
+        where TResponse : ServiceResponseBase, new()
+    {
+        protected readonly ILog _log = LogManager.GetLogger("ServerLogger");
+
+        protected abstract void DoServiceExecute(TRequest request);
+        protected abstract void FillResponse(TRequest request, TResponse response);
+        protected abstract void ValidateRequest(TRequest request);
+
+        public IServiceResponse Execute(IServiceRequest request)
+        {
+            IServiceResponse response;
+            var requestObject = request as TRequest;
+
+            try
+            {
+                ValidateRequest(requestObject);
+                DoServiceExecute(requestObject);
+                response = GenerateResponse(requestObject);
+            }
+            catch (Exception e)
+            {
+                _log.ErrorFormat("Service exception on request (id: {0}) for service: {1}", request.RequestId, request.ServiceName);
+                _log.DebugFormat("Exception data: {0}{1}", Environment.NewLine, e);
+
+                // TODO: I could call a factory to generate an error response 
+                response = BuildErrorResponse(request);
+            }
+
+            return response;
+        }
+
+        private ServiceErrorResponse BuildErrorResponse(IServiceRequest request)
+        {
+            var response = new ServiceErrorResponse()
+            {
+                ServiceName = request.ServiceName,
+                RequestId = request.RequestId,
+                Status = false,
+                ErrorCode = 99,
+                Message = String.Format("Error while handling request for service: {0}", request.ServiceName)
+            };
+            return response;
+        }
+
+        private TResponse GenerateResponse(TRequest request)
+        {
+            var response = new TResponse();
+            response.RequestId = request.RequestId;
+            response.ServiceName = request.ServiceName;
+            FillResponse(request, response);
+            return response;
+        }
+    }
+}
---
+++ PayCalculator/PayCalculator.core.BusinessServices/Employee/EmployeeAddService.cs
@@ -0,0 +1,45 @@
+﻿using PayCalculator.Contracts.Employee;
+using PayCalculator.core.BusinessServices.Common;
+using PayCalculator.core.Model.Employee;
+using System;
+
+namespace PayCalculator.core.BusinessServices.Employee
+{
+    public class EmployeeAddService : BusinessServiceBase<EmployeeAddServiceRequest, EmployeeAddServiceResponse>
+    {
+
+        private readonly IEmployeeDao _employeeDao;
+
+        public EmployeeAddService(IEmployeeDao employeeDao)
+        {
+            _employeeDao = employeeDao;
+        }
+
+        protected override void DoServiceExecute(EmployeeAddServiceRequest request)
+        {
+            _log.DebugFormat("Request to add new employee: {0}", request.EmployeeName);
+            var employee = _employeeDao.GetOrCreateNew(request.EmployeeName);
+            UpdateEmployeeData(employee, request);
+            _employeeDao.Save(employee);
+        }
+
+        private void UpdateEmployeeData(IEmployee employee, EmployeeAddServiceRequest request)
+        {
+            employee.Init(request.EmployeeName, request.EmployeeLocation, request.GrossSalary);
+        }
+
+        protected override void ValidateRequest(EmployeeAddServiceRequest request)
+        {
+            if (String.IsNullOrEmpty(request.EmployeeName) == true)
+            {
+                throw new Exception("Employee name cannot be empty");
+            }
+        }
+
+        protected override void FillResponse(EmployeeAddServiceRequest request, EmployeeAddServiceResponse response)
+        {
+            response.Status = true;
+            response.Message = "Employee added successfully";
+        }
+    }
+}
