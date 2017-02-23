f1091e457ebc1a8a8ac316a1b75c98cc05c77987
adir-ch <actiger@gmail.com>
2017-02-23 14:06:50 +1100

    Fixed the test cases

--- PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects.Test/Tax/DefaultTaxCalculatorShould.cs
+++ PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects.Test/Tax/DefaultTaxCalculatorShould.cs
@@ -12,10 +12,10 @@ namespace PayCalculator.core.BusinessObjects.Test.Tax
     public class DefaultTaxCalculatorShould
     {
         [Test]
-        [TestCase(200000, ExpectedResult = 25)]
-        [TestCase(0, ExpectedResult = 50)]
+        [TestCase(200000, ExpectedResult = 63232)]
+        [TestCase(0, ExpectedResult = 0)]
         [TestCase(18201, ExpectedResult = 0)]
-        [TestCase(85000, ExpectedResult = 7)]
+        [TestCase(85000, ExpectedResult = 19172)]
         public decimal CalculateTaxBrackets(decimal taxableIncom)
         {
             DefaultTaxCalculator calculator = new DefaultTaxCalculator();
