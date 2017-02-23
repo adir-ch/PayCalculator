ff40b68469995e119e9d09cb27d0c0c19441e033
adir-ch <actiger@gmail.com>
2017-02-23 12:58:23 +1100

    First DefaultTaxDeductionRule test before refactoring

---
+++ PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects.Test/Salary/DefaultTaxDeductionRuleShould.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using PayCalculator.core.BusinessComponents.Salary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.core.BusinessObjects.Test.Salary
+{
+    [TestFixture]
+    public class DefaultTaxDeductionRuleShould
+    {
+        private DefaultTaxDeductionRule _defaultTaxDeductionRule;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _defaultTaxDeductionRule = new DefaultTaxDeductionRule(); 
+        }
+
+        [Test]
+        public void ReturnEmpltyDescription()
+        {
+            Assert.IsTrue(String.IsNullOrEmpty(_defaultTaxDeductionRule.GetRuleDescription()) == false);
+        }
+
+        [Test]
+        public void ReturnDefaultTaxDeductionRuleDescription()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
---
+++ PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects/Salary/DefaultTaxDeductionRule.cs
@@ -0,0 +1,21 @@
+﻿using PayCalculator.core.Model.Salary;
+using PayCalculator.core.Model.Tax;
+using System;
+
+namespace PayCalculator.core.BusinessComponents.Salary
+{
+    public class DefaultTaxDeductionRule : IDeductionRule
+    {
+        public string RuleName { get; set; }
+
+        public string GetRuleDescription()
+        {
+            return String.Empty;
+        }
+
+        public decimal Apply(decimal taxableIncome)
+        {
+            return 0; 
+        }
+    }
+}
