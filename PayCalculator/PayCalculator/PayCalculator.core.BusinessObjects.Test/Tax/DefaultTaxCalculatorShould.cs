e64734461d9810aab2148b6bdc14955c5600a2f4
adir-ch <actiger@gmail.com>
2017-02-23 14:02:38 +1100

    Added DefaultTaxCalculator + test. Test is failing - starting refactor

---
+++ PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects.Test/Tax/DefaultTaxCalculatorShould.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using PayCalculator.core.BusinessComponents.Tax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.core.BusinessObjects.Test.Tax
+{
+    [TestFixture]
+    public class DefaultTaxCalculatorShould
+    {
+        [Test]
+        [TestCase(200000, ExpectedResult = 25)]
+        [TestCase(0, ExpectedResult = 50)]
+        [TestCase(18201, ExpectedResult = 0)]
+        [TestCase(85000, ExpectedResult = 7)]
+        public decimal CalculateTaxBrackets(decimal taxableIncom)
+        {
+            DefaultTaxCalculator calculator = new DefaultTaxCalculator();
+            return calculator.CalculateTax(taxableIncom); 
+        }
+    }
+}
---
+++ PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects/Tax/DefaultTaxCalculator.cs
@@ -0,0 +1,39 @@
+﻿using PayCalculator.core.Model.Tax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PayCalculator.core.BusinessComponents.Tax
+{
+    public class DefaultTaxCalculator : ITaxCalculator
+    {
+        private List<ITaxBracket> _taxBracets;
+
+        public DefaultTaxCalculator()
+        {
+            _taxBracets = new List<ITaxBracket>();
+
+            // take calculator bracets from DB
+            // string bracketsDescription = DB.GetBracketsDescription("DefaultBrackets"); 
+            // InitBracets(bracketsDescription); 
+
+            InitBracets("DefaultBrackets");
+        }
+
+        public decimal CalculateTax(decimal taxableIncome)
+        {
+            return 0; 
+        }
+
+        private void InitBracets(string description)
+        {
+            // TODO: bracets should be taken from DB, using description
+            //_taxBracets = DB.GetTaxBracets(description);
+            _taxBracets.Add(new DefaultTaxBracket(0, 18200, 0));
+            _taxBracets.Add(new DefaultTaxBracket(18201, 37000, (decimal)0.19));
+            _taxBracets.Add(new DefaultTaxBracket(37001, 87000, (decimal)0.325));
+            _taxBracets.Add(new DefaultTaxBracket(87001, 180000, (decimal)0.37));
+            _taxBracets.Add(new DefaultTaxBracket(180001, decimal.MaxValue, (decimal)0.45));
+        }
+    }
+}
