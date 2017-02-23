76c09a2f0873dea7019602b474e397c24f7f0c0e
adir-ch <actiger@gmail.com>
2017-02-23 13:27:09 +1100

    DefaultTaxBracet + test added. Test is failing - starting to refactor the DefaultTaxBracet

---
+++ PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects.Test/Tax/DefaultTaxBracketShould.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using PayCalculator.core.BusinessComponents.Tax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.core.BusinessObjects.Test.Tax
+{
+    [TestFixture]
+    public class DefaultTaxBracketShould
+    {
+        [Test]
+        [TestCase(0, 100, 0.5, 50, ExpectedResult = true)]
+        public bool CheckIfTaxableIncomeInTaxBracet(decimal lowerBand, decimal upperBand, decimal taxRate, decimal taxableIncome)
+        {
+            DefaultTaxBracket bracet = new DefaultTaxBracket(lowerBand, upperBand, taxRate);
+            return bracet.IsInBracket(taxableIncome); 
+        }
+    }
+}
---
+++ PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects/Tax/DefaultTaxBracket.cs
@@ -0,0 +1,33 @@
+﻿using PayCalculator.core.Model.Tax;
+
+namespace PayCalculator.core.BusinessComponents.Tax
+{
+    public class DefaultTaxBracket : ITaxBracet
+    {
+        private readonly decimal _lowerBand;
+        private readonly decimal _upperBand;
+        private readonly decimal _taxRate;
+
+        public DefaultTaxBracket(decimal lowerBand, decimal upperBand, decimal taxRate)
+        {
+            _lowerBand = lowerBand;
+            _upperBand = upperBand;
+            _taxRate = taxRate;
+        }
+
+        public decimal CalculateTaxBracet(decimal taxableIncome)
+        {
+            return 0;
+        }
+
+        public bool CanApplyBracet(decimal taxableIncome)
+        {
+            return false; 
+        }
+
+        public bool IsInBracket(decimal taxableIncome)
+        {
+            return false; 
+        }
+    }
+}
