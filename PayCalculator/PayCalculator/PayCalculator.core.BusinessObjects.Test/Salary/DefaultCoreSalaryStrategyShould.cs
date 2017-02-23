9007a0ed183895496c36fa59aef7d14772e043a2
adir-ch <actiger@gmail.com>
2017-02-24 00:55:13 +1100

    New DefaultCoreSalaryStrategy and a test

---
+++ PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects.Test/Salary/DefaultCoreSalaryStrategyShould.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.core.BusinessObjects.Test.Salary
+{
+    [TestFixture]
+    public class DefaultCoreSalaryStrategyShould
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
---
+++ PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects/Salary/DefaultCoreSalaryStrategy.cs
@@ -0,0 +1,19 @@
+﻿using PayCalculator.core.Model.Salary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.core.BusinessObjects.Salary
+{
+    public class DefaultCoreSalaryStrategy : ISalaryStrategy
+    {
+        public decimal GrossSalary { get; set; }
+
+        public ISalary Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
