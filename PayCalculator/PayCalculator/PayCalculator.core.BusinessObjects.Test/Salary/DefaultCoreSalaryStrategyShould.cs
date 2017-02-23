c8933f5f8c3ad46730b15caaa8ba1de5e8830c71
adir-ch <actiger@gmail.com>
2017-02-24 10:43:05 +1100

    Test is failing - refactoring

--- PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects.Test/Salary/DefaultCoreSalaryStrategyShould.cs
+++ PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects.Test/Salary/DefaultCoreSalaryStrategyShould.cs
@@ -1,29 +1,51 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
+using bc = PayCalculator.core.BusinessObjects.Salary;
 using PayCalculator.core.BusinessObjects.Salary;
+using PayCalculator.core.Model.Salary;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PayCalculator.core.BusinessObjects.Test.Salary
 {
     [TestFixture]
     public class DefaultCoreSalaryStrategyShould
     {
-        private DefaultCoreSalaryStrategy _coreSalaryStragegy;
-
+        private DefaultCoreSalaryStrategy _coreSalaryStrategy;
+        
         [OneTimeSetUp]
         public void Init()
         {
-            _coreSalaryStragegy = new DefaultCoreSalaryStrategy();
+            _coreSalaryStrategy = new DefaultCoreSalaryStrategy();
         }
 
         [Test]
         public void InitGrossSalary([Values(0, 5000)] decimal grossSalary)
         {
-            _coreSalaryStragegy.GrossSalary = grossSalary;
-            Assert.AreEqual(_coreSalaryStragegy.GrossSalary, grossSalary); 
+            _coreSalaryStrategy.GrossSalary = grossSalary;
+            Assert.AreEqual(_coreSalaryStrategy.GrossSalary, grossSalary); 
+        }
+
+        [Test]
+        public void ExecuteStrategyAndReturnSalaryObject([Values(0, 5000)] decimal grossSalary)
+        {
+            _coreSalaryStrategy.GrossSalary = grossSalary; 
+            ISalary salary = new bc.Salary();
+            salary.GrossSalary = grossSalary;
+            salary.TaxableIncome = grossSalary; 
+
+            var deductionsList = new List<Tuple<string, decimal>>() { Tuple.Create<string, decimal>("a", (decimal)100),
+                                                                      Tuple.Create<string, decimal>("b", (decimal)200),
+                                                                      Tuple.Create<string, decimal>("c", (decimal)300)};
+
+            salary.Deductions = deductionsList;
+            salary.NetAnnualSalary = (salary.TaxableIncome - 600); 
+            ISalary calculatedSalary = _coreSalaryStrategy.Execute(); 
+            Assert.AreEqual(salary, calculatedSalary);
         }
     }
 }
