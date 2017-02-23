86778c870405a680505ce3f12ff54ceff63014bb
adir-ch <actiger@gmail.com>
2017-02-23 12:35:07 +1100

    DeductionsBase + DeductionsBase initial test

---
+++ PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects.Test/Salary/DeductionsBaseShould.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using PayCalculator.core.BusinessObjects.Salary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.core.BusinessObjects.Test.Salary
+{
+    [TestFixture]
+    public class DeductionsBaseShould
+    {
+        private DeductionsBase _deductionsBase;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _deductionsBase = new DeductionsBase(); 
+        }
+
+        [Test]
+        public void ReturnDeductionReport()
+        {
+            Assert.IsNotEmpty(_deductionsBase.GetDeductionsReport());
+        }
+    }
+}
---
+++ PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects/Salary/DeductionsBase.cs
@@ -0,0 +1,33 @@
+﻿using log4net;
+using PayCalculator.core.Model.Salary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.core.BusinessObjects.Salary
+{
+    public class DeductionsBase : IDeductions
+    {
+        protected readonly ILog _log = LogManager.GetLogger("Deductions");
+        protected IList<IDeductionRule> _deductionRules;
+        protected IList<Tuple<string, decimal>> _deductionsReport;
+
+        public DeductionsBase()
+        {
+            _deductionRules = new List<IDeductionRule>();
+            _deductionsReport = new List<Tuple<string, decimal>>(); 
+        }
+
+        public decimal GetTotalDeductionsAmount(decimal taxableIncome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Tuple<string, decimal>> GetDeductionsReport()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
