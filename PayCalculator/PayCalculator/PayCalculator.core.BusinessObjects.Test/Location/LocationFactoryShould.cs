3a373437723a693e3ace6764683d3fd3b06a75b6
adir-ch <actiger@gmail.com>
2017-02-24 12:22:01 +1100

    LocationFactoryShould tests are failing - refactoring

---
+++ PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects.Test/Location/LocationFactoryShould.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using PayCalculator.core.BusinessObjects.Location;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.core.BusinessObjects.Test.Location
+{
+    [TestFixture]
+    public class LocationFactoryShould
+    {
+        private LocationFactory _locationFactory;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _locationFactory = new LocationFactory();
+        }
+
+        [Test]
+        public void CreateAndAddNewLocation([Values("location1", "location2")] string location)
+        {
+            var createdLocation = _locationFactory.CreateLocation(location);
+            Assert.AreEqual(location, createdLocation.LocationName);
+        }
+    }
+}
---
+++ PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects/Location/LocationFactory.cs
@@ -0,0 +1,17 @@
+﻿using PayCalculator.core.Model.Location;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.core.BusinessObjects.Location
+{
+    public class LocationFactory
+    {
+        public ILocation CreateLocation(string locationName)
+        {
+            return null; 
+        }
+    }
+}
