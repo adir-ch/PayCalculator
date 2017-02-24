6cc72c4946ef5b83505276552783921fca86aced
adir-ch <actiger@gmail.com>
2017-02-24 11:50:59 +1100

    DefaultCoreLocation tests are passing (since it is only  the inteface implementations) - continue writing tests

---
+++ PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects.Test/Location/DefaultCoreLocationShould.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using PayCalculator.core.BusinessObjects.Location;
+using PayCalculator.Infra.IoC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.core.BusinessObjects.Test.Location
+{
+    [TestFixture]
+    public class DefaultCoreLocationShould
+    {
+        private DefaultCoreLocation _defaultCoreLocation;
+        private Injector _injector; 
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _injector = Injector.Instance;
+            _defaultCoreLocation = new DefaultCoreLocation(); 
+        }
+
+        [Test]
+        public void StoreLocationName()
+        {
+            string locationName = "AnyLocation";
+            _defaultCoreLocation.LocationName = locationName;
+
+            Assert.AreEqual(locationName, _defaultCoreLocation.LocationName);
+        }
+    }
+}
---
+++ PayCalculator/PayCalculator/PayCalculator.core.BusinessObjects/Location/DefaultCoreLocation.cs
@@ -0,0 +1,20 @@
+﻿using PayCalculator.core.Model.Location;
+using PayCalculator.core.Model.Salary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.core.BusinessObjects.Location
+{
+    public class DefaultCoreLocation : ILocation
+    {
+        public string LocationName { get; set; }
+
+        public ISalaryStrategy GetLocationSalaryStrategy()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
