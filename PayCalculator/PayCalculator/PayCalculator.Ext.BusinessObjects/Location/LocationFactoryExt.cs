3d73b9faf783ad381c2d66279aed060d5bd0a193
adir-ch <actiger@gmail.com>
2017-02-25 19:51:18 +1100

    Extended LocationFactory + tests

---
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects/Location/LocationFactoryExt.cs
@@ -0,0 +1,18 @@
+﻿using PayCalculator.core.BusinessObjects.Location;
+using PayCalculator.core.Model.Location;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.Ext.BusinessObjects.Location
+{
+    public class LocationFactoryExt : LocationFactory
+    {
+        public override ILocation CreateLocation(string locationName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
