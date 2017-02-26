d32a81aba3909a5f0819d4351a3dc7fee79ad95b
adir-ch <actiger@gmail.com>
2017-02-26 15:16:12 +1100

    All initial tests are passing, continue with other tests.

---
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects.Test/Salary/AustraliaSalaryStrategyShould.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using NUnit.Framework;
+using PayCalculator.core.Model.Salary;
+using PayCalculator.Ext.BusinessObjects.Location;
+using PayCalculator.Infra.IoC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.Ext.BusinessObjects.Test.Salary
+{
+    [TestFixture]
+    public class AustraliaSalaryStrategyShould
+    {
+        private AustraliaSalaryStrategy _australiaSalaryStrategy;
+        private Mock<IDeductions> _deductionsMock;
+        private Injector _injector;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _deductionsMock = new Mock<IDeductions>();
+            _injector = Injector.Instance;
+            _injector.RegisterInstance<IDeductions>(_deductionsMock.Object, "AustraliaSalaryDeductions");
+            _australiaSalaryStrategy = new AustraliaSalaryStrategy();
+        }
+
+        [Test]
+        public void InitGrossSalary([Values(0, 5000)] decimal grossSalary)
+        {
+            _australiaSalaryStrategy.GrossSalary = grossSalary;
+            Assert.AreEqual(_australiaSalaryStrategy.GrossSalary, grossSalary);
+        }
+    }
+}
--- PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects/Location/AustraliaSalaryStrategy.cs
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects/Location/AustraliaSalaryStrategy.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using PayCalculator.core.Model.Salary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace PayCalculator.Ext.BusinessObjects.Location
 {
-    public class AustraliaSalaryStrategy
+    public class AustraliaSalaryStrategy : ISalaryStrategy
     {
+        public decimal GrossSalary { get; set; }
+
+        public ISalary Execute()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
