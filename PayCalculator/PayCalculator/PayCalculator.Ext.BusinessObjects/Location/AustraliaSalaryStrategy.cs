7dd11983563b0ef30c82ed89a4acb239efbd7144
adir-ch <actiger@gmail.com>
2017-02-26 16:10:44 +1100

    refactored - tests are still failing. Continue refactoring. Test is failing on deduction object, need to create one and inject to Australia salary strategy

--- PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects/Location/AustraliaSalaryStrategy.cs
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects/Location/AustraliaSalaryStrategy.cs
@@ -9,10 +9,29 @@ namespace PayCalculator.Ext.BusinessObjects.Location
     public class AustraliaSalaryStrategy : ISalaryStrategy
     {
         public decimal GrossSalary { get; set; }
+        private IDeductions _deductions;
+
+        public AustraliaSalaryStrategy()
+        {
+
+        }
 
         public ISalary Execute()
         {
+            var superannuation = CalculateSuperannuation();
+            var taxableIncome = (GrossSalary - superannuation);
+            var netAnnualSalary = taxableIncome - _deductions.GetTotalDeductionsAmount(taxableIncome);
+            return BuildSalaryReport(superannuation, taxableIncome, netAnnualSalary);
+        }
+
+        private ISalary BuildSalaryReport(decimal superannuation, decimal taxableIncome, decimal netAnnualSalary)
+        {
             throw new NotImplementedException();
+        }
+
+        private decimal CalculateSuperannuation()
+        {
+            return (GrossSalary * (decimal)0.086758); // take this value from DB
         }
     }
 }
