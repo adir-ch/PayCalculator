fb5aa66786bcb341424d3dfd306abdb65d31ddad
adir-ch <actiger@gmail.com>
2017-02-26 16:21:41 +1100

    Created AustraliaSalaryDeductions and imlpemented the core IDeductions interface

---
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects/Salary/AustraliaSalaryDeductions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.Ext.BusinessObjects.Salary
+{
+    class AustraliaSalaryDeductions : IAustraliaSalaryDeductions
+    {
+        decimal GetTotalDeductionsAmount(decimal taxableIncome)
+        {
+            throw new NotImplementedException();
+        }
+        IList<Tuple<string, decimal>> GetDeductionsReport()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
