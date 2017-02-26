6ab3ad6630792804e95e83ee45e49bb6103f776e
adir-ch <actiger@gmail.com>
2017-02-26 16:07:09 +1100

    Tests are failing - refactoring

--- PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects.Test/Salary/AustraliaSalaryStrategyShould.cs
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects.Test/Salary/AustraliaSalaryStrategyShould.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using PayCalculator.core.Model.Salary;
 using PayCalculator.Ext.BusinessObjects.Location;
+using bc = PayCalculator.core.BusinessObjects.Salary;
 using PayCalculator.Infra.IoC;
 using System;
 using System.Collections.Generic;
@@ -11,12 +12,21 @@ using System.Threading.Tasks;
 
 namespace PayCalculator.Ext.BusinessObjects.Test.Salary
 {
-    [TestFixture]
+    [TestFixture(0)]
+    [TestFixture(1000)]
+    [TestFixture(5000)]
     public class AustraliaSalaryStrategyShould
     {
+        private decimal _initialGrossSalary; 
         private AustraliaSalaryStrategy _australiaSalaryStrategy;
         private Mock<IDeductions> _deductionsMock;
         private Injector _injector;
+        private ISalary _salary;
+
+        public AustraliaSalaryStrategyShould(int grossSalary)
+        {
+            _initialGrossSalary = grossSalary; 
+        }
 
         [OneTimeSetUp]
         public void Init()
@@ -27,11 +37,44 @@ namespace PayCalculator.Ext.BusinessObjects.Test.Salary
             _australiaSalaryStrategy = new AustraliaSalaryStrategy();
         }
 
-        [Test]
-        public void InitGrossSalary([Values(0, 5000)] decimal grossSalary)
+        [SetUp]
+        public void BeforeEach()
         {
-            _australiaSalaryStrategy.GrossSalary = grossSalary;
-            Assert.AreEqual(_australiaSalaryStrategy.GrossSalary, grossSalary);
+            _salary = new bc.Salary();
+            _salary.GrossSalary = _initialGrossSalary;
+
+            decimal super = _salary.GrossSalary - (_salary.GrossSalary * (decimal)0.086758);
+            _salary.TaxableIncome = _salary.GrossSalary - super;
+
+            decimal deductionsAmount = (decimal)600;
+            var deductionsList = new List<Tuple<string, decimal>>() { Tuple.Create<string, decimal>("a", (decimal)100),
+                                                                      Tuple.Create<string, decimal>("b", (decimal)200),
+                                                                      Tuple.Create<string, decimal>("c", (decimal)300)};
+
+            _deductionsMock.Setup(d => d.GetDeductionsReport()).Returns(deductionsList);
+            _deductionsMock.Setup(d => d.GetTotalDeductionsAmount(It.IsAny<decimal>())).Returns(deductionsAmount);
+
+            _salary.Deductions = deductionsList;
+            _salary.NetAnnualSalary = (_salary.TaxableIncome - deductionsAmount);
+        }
+
+        [Test]
+        public void InitGrossSalary()
+        {
+            _australiaSalaryStrategy.GrossSalary = _initialGrossSalary;
+            Assert.AreEqual(_australiaSalaryStrategy.GrossSalary, _initialGrossSalary);
+        }
+
+        [Test]
+        public void ExecuteStrategyAndReturnSalaryObject()
+        {
+            _australiaSalaryStrategy.GrossSalary = _initialGrossSalary;
+            ISalary calculatedSalary = _australiaSalaryStrategy.Execute();
+
+            Assert.AreEqual(_salary.GrossSalary, calculatedSalary.GrossSalary);
+            Assert.AreEqual(_salary.TaxableIncome, calculatedSalary.TaxableIncome);
+            Assert.AreEqual(_salary.NetAnnualSalary, calculatedSalary.NetAnnualSalary);
+            Assert.AreEqual(_salary.Deductions, calculatedSalary.Deductions);
         }
     }
 }
