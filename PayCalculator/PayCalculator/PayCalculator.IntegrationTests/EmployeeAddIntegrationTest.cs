ae7064cbdc86748f1ef993d2007191c7c9c79fe1
adir-ch <actiger@gmail.com>
2017-02-27 22:18:38 +1100

    EmployeeAddServiceIntegrationTess - finished

---
+++ PayCalculator/PayCalculator/PayCalculator.IntegrationTests/EmployeeAddIntegrationTest.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using PayCalculator.Contracts.Employee;
+using PayCalculator.Infra.IoC;
+using PayCalculator.Infra.WebApi;
+
+namespace PayCalculator.IntegrationTests
+{
+    [TestFixture]
+    public class EmployeeAddIntegrationTest
+    {
+        private PayCalculatorWebApi _webApi;
+        private Injector _injector;
+        private EmployeeAddServiceRequest _sentRequest;
+        private EmployeeAddServiceResponse _expectedResponse;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _injector = Injector.Instance; 
+            _webApi = PayCalculatorWebApi.Instance;
+            _sentRequest = BuildEmployeeAddRequest();
+            _expectedResponse = BuildEmployeeAddExpectedRespose(); 
+        }
+
+        [Test]
+        public void AddEmployeeIntegrationTest()
+        {
+            var response = _webApi.CallService(_sentRequest) as EmployeeAddServiceResponse;
+            Assert.That(response.RequestId, Is.EqualTo(_expectedResponse.RequestId));
+            Assert.That(response.Status, Is.EqualTo(_expectedResponse.Status));
+            Assert.That(response.Message, Is.EqualTo(_expectedResponse.Message));
+            Assert.That(response.ServiceName, Is.EqualTo(_expectedResponse.ServiceName)); 
+        }
+
+        private EmployeeAddServiceResponse BuildEmployeeAddExpectedRespose()
+        {
+            return new EmployeeAddServiceResponse()
+            {
+                Status = true, 
+                ServiceName = "EmployeeAddService",
+                Message = "Employee added successfully",
+                RequestId = "12345"
+            };
+        }
+
+        private EmployeeAddServiceRequest BuildEmployeeAddRequest()
+        {
+            return new EmployeeAddServiceRequest()
+            {
+                EmployeeName = "Adir",
+                EmployeeLocation = "Australia",
+                GrossSalary = "200000",
+                RequestId = "12345"
+            };
+        }
+    }
+}
