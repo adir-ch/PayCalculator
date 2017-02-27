e668754533d2d76cb79ccdde566a099445679d43
adir-ch <actiger@gmail.com>
2017-02-27 14:16:49 +1100

    Some tests is failing - refactoring

--- PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects/Tax/Australia/IncomeTaxCalculator.cs
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects/Tax/Australia/IncomeTaxCalculator.cs
@@ -1,13 +1,33 @@
-﻿using PayCalculator.core.Model.Tax;
+﻿using log4net;
+using PayCalculator.core.Model.Tax;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PayCalculator.Ext.BusinessObjects.Tax.Australia
 {
-    public class IncomeTaxCalculator : ITaxCalculator
+    public class IncomeTaxCalculator : ITaxCalculator 
     {
+        protected readonly ILog _log = LogManager.GetLogger("AU Income tax calc");
+        private List<ITaxBracket> _taxBrackets;
+
+        public IncomeTaxCalculator()
+        {
+            _taxBrackets = new List<ITaxBracket>();
+            InitBracets("AustraliaIncomeTaxBrackets"); 
+        }
+        
         public decimal CalculateTax(decimal taxableIncome)
         {
-            throw new NotImplementedException();
+            decimal totalCalculatedIncomeTax = 0; 
+            decimal currentBracet = 0;
+            _log.DebugFormat("Calculated total income tax: {0}", totalCalculatedIncomeTax);
+            return totalCalculatedIncomeTax;
+        }
+
+        private void InitBracets(string description)
+        {
+            
         }
     }
 }
