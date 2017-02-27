44d7a6f425f60c3d0fa98400f5961801f7de2216
adir-ch <actiger@gmail.com>
2017-02-27 13:13:34 +1100

    New Class and test files added

---
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects.Test/Salary/Australia/BudgetRepairTaxDeductionRuleShould.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using NUnit.Framework;
+using PayCalculator.core.Model.Tax;
+using PayCalculator.Ext.BusinessObjects.Salary.Australia.DeductionRules;
+using PayCalculator.Infra.IoC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.Ext.BusinessObjects.Test.Salary.Australia
+{
+    [TestFixture]
+    public class BudgetRepairTaxDeductionRuleShould
+    {
+        private BudgetRepairTaxDeductionRule _deductionRule;
+        private Injector _injector;
+        private Mock<ITaxCalculator> _budgetRepairTaxCalculatorMock;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _injector = Injector.Instance;
+            _budgetRepairTaxCalculatorMock = new Mock<ITaxCalculator>();
+            _injector.RegisterInstance<ITaxCalculator>(_budgetRepairTaxCalculatorMock.Object, "AustraliaBudgetRepairTaxCalculator");
+        }
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            _deductionRule = new BudgetRepairTaxDeductionRule();
+        }
+    }
+}
