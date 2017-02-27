0ed461a976194d2b056891ca3adb81452ece56bc
adir-ch <actiger@gmail.com>
2017-02-27 15:40:54 +1100

    Code refactored, but some tests are failing - continue refactoring

--- PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects/Tax/Australia/MedicareLevyTaxCalculator.cs
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects/Tax/Australia/MedicareLevyTaxCalculator.cs
@@ -1,4 +1,5 @@
-﻿using PayCalculator.core.Model.Tax;
+﻿using PayCalculator.core.BusinessObjects.Tax;
+using PayCalculator.core.Model.Tax;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,29 @@ namespace PayCalculator.Ext.BusinessObjects.Tax.Australia
 {
     public class MedicareLevyTaxCalculator : ITaxCalculator
     {
+        private List<ITaxBracket> _taxBrackets;
+
+        public MedicareLevyTaxCalculator()
+        {
+            _taxBrackets = new List<ITaxBracket>();
+            InitBracets("AustraliaMedicareBracets"); 
+        }
+
         public decimal CalculateTax(decimal taxableIncome)
         {
-            decimal totalCalculatedMedicareLevy = 0;
-            return totalCalculatedMedicareLevy;
+            decimal totalCalculatedMedicareLevy = _taxBrackets
+                 .Where(bracket => bracket.CanApplyBracket(taxableIncome))
+                 .Sum(validBracket => validBracket.CalculateTaxBracket(taxableIncome));
+
+            return Math.Round(totalCalculatedMedicareLevy);
+        }
+
+        private void InitBracets(string description)
+        {
+            // TODO: Bracets should be taken from DB
+            _taxBrackets.Add(new DefaultTaxBracket(0, 21335, 0));
+            _taxBrackets.Add(new DefaultTaxBracket(21336, 26668, (decimal)0.1));
+            _taxBrackets.Add(new DefaultTaxBracket(26669, decimal.MaxValue, (decimal)0.02));
         }
     }
 }
