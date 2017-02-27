350a30f95c2a0f5a09cf760c6da5d03840a913e8
adir-ch <actiger@gmail.com>
2017-02-27 14:50:03 +1100

    Class and tests added. Test is failing - refactoring

---
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects.Test/Tax/Australia/BudgetRepairTaxCalculatorShould.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using PayCalculator.Ext.BusinessObjects.Tax.Australia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.Ext.BusinessObjects.Test.Tax.Australia
+{
+    [TestFixture]
+    public class BudgetRepairTaxCalculatorShould
+    {
+        [Test]
+        [TestCase(200000, ExpectedResult = 63232)]
+        [TestCase(0, ExpectedResult = 0)]
+        [TestCase(18201, ExpectedResult = 0)]
+        [TestCase(85000, ExpectedResult = 19172)]
+        public decimal CalculateBudgetRepairTax(decimal taxableIncome)
+        {
+            var budgetRepairTaxCalculator = new BudgetRepairTaxCalculator();
+            return budgetRepairTaxCalculator.CalculateTax(taxableIncome);
+        }
+    }
+}
