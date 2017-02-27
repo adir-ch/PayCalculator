144a65b35c94fe8b180be2caf61934001cb516ca
adir-ch <actiger@gmail.com>
2017-02-27 15:19:02 +1100

    Class and tests added. Test is failing - refactoring

---
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects.Test/Tax/Australia/MedicareLevyTaxCalculatorShould.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PayCalculator.Ext.BusinessObjects.Tax.Australia;
+
+namespace PayCalculator.Ext.BusinessObjects.Test.Tax.Australia
+{
+    [TestFixture]
+    public class MedicareLevyTaxCalculatorShould
+    {
+        [Test]
+        [TestCase(0, ExpectedResult = 0)]
+        [TestCase(21335, ExpectedResult = 0)]
+        [TestCase(23000, ExpectedResult = 167)]
+        [TestCase(26668, ExpectedResult = 533)]
+        [TestCase(50000, ExpectedResult = 1000)]
+        public decimal CalculateMedicareLevy(decimal taxableIncome)
+        {
+            return new MedicareLevyTaxCalculator().CalculateTax(taxableIncome); 
+        }
+    }
+}
