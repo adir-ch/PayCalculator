09fcea9886ac31831c1c2bc2e118417582abc5b0
adir-ch <actiger@gmail.com>
2017-02-28 00:07:11 +1100

    Germany salary strategy + deductions

---
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects/Salary/Germany/GermanySalaryDeductions.cs
@@ -0,0 +1,28 @@
+﻿using PayCalculator.core.BusinessObjects.Salary;
+using PayCalculator.core.Model.Salary;
+using PayCalculator.Ext.Model.Salary;
+using PayCalculator.Infra.IoC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.Ext.BusinessObjects.Salary.Germany
+{
+    public class GermanySalaryDeductions : DeductionsBase, IGermanySalaryDeductions
+    {
+        protected override void SetDeductionRules()
+        {
+            // rules should be taken from DB or another micro service 
+            // var rules = DB.GetAsutraliaSalaryDeductionsRules(); 
+            // var rules = WebApi.GetConfigurationService("AsutraliaSalaryDeductionsRules"); 
+
+            var fetchedRules = new List<string>() { "IncomeTaxDeductionRule", 
+                                                    "SolidaritySurchargeDeductionRule" };
+
+            var rules = fetchedRules.Select(ruleName => Injector.Instance.Inject<IDeductionRule>(ruleName));
+            _deductionRules = rules.ToList();
+        }
+    }
+}
---
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects/Salary/Germany/GermanySalaryStrategy.cs
@@ -0,0 +1,35 @@
+﻿using PayCalculator.core.Model.Salary;
+using PayCalculator.Ext.Model.Salary;
+using cbc = PayCalculator.core.BusinessObjects.Salary;
+
+namespace PayCalculator.Ext.BusinessObjects.Salary.Germany
+{
+    public class GermanySalaryStrategy : ISalaryStrategy
+    {
+        private IDeductions _deductions;
+
+        public GermanySalaryStrategy(IGermanySalaryDeductions deductions)
+        {
+            _deductions = deductions;
+        }
+
+        public decimal GrossSalary { get; set; }
+
+        public ISalary Execute()
+        {
+            var taxableIncome = GrossSalary;
+            var netAnnualSalary = taxableIncome - _deductions.GetTotalDeductionsAmount(taxableIncome);
+            return BuildSalaryReport(taxableIncome, netAnnualSalary);
+        }
+
+        private ISalary BuildSalaryReport(decimal taxableIncome, decimal netAnnualSalary)
+        {
+            cbc.Salary salary = new cbc.Salary();
+            salary.GrossSalary = GrossSalary;
+            salary.TaxableIncome = taxableIncome;
+            salary.NetAnnualSalary = netAnnualSalary;
+            salary.Deductions = _deductions.GetDeductionsReport();
+            return salary;
+        }
+    }
+}
