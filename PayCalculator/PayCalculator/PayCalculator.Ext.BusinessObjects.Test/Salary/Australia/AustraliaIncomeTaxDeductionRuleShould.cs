148985d19f8213634798a107d4bd160b8c1383a6
adir-ch <actiger@gmail.com>
2017-02-27 12:33:17 +1100

    Test is failing - refactoring

--- PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects.Test/Salary/Australia/AustraliaIncomeTaxDeductionRuleShould.cs
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects.Test/Salary/Australia/AustraliaIncomeTaxDeductionRuleShould.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PayCalculator.Ext.BusinessObjects.Salary.Australia.DeductionRules; 
+using PayCalculator.Ext.BusinessObjects.Salary.Australia.DeductionRules;
+using PayCalculator.Infra.IoC;
+using PayCalculator.core.Model.Tax;
+using Moq; 
 
 namespace PayCalculator.Ext.BusinessObjects.Test.Salary.Australia
 {
@@ -12,17 +15,31 @@ namespace PayCalculator.Ext.BusinessObjects.Test.Salary.Australia
     public class AustraliaIncomeTaxDeductionRuleShould
     {
         private IncomeTaxDeductionRule _deductionRule;
+        private Injector _injector;
+        private Mock<ITaxCalculator> _taxCalcularotMock; 
 
         [OneTimeSetUp]
         public void Init()
         {
-            _deductionRule = new IncomeTaxDeductionRule(); 
+            _injector = Injector.Instance;
+            _taxCalcularotMock = new Mock<ITaxCalculator>(); 
+            _deductionRule = new IncomeTaxDeductionRule();
+            _injector.RegisterInstance<ITaxCalculator>(_taxCalcularotMock.Object, "AustraliaIncomeTaxCalculator"); 
         }
 
         [Test]
         public void SetAustralianTaxRuleDescription()
         {
             Assert.AreEqual(_deductionRule.GetRuleDescription(), "Income Tax"); 
+        }
+
+        [Test]
+        public void CallCalculateTaxOnRuleApply([Values(0, 1000, 5000)] decimal taxableIncome)
+        {
+            _taxCalcularotMock.Setup(calc => calc.CalculateTax(taxableIncome)).Returns(100);
+            var taxDeduction = _deductionRule.Apply(taxableIncome);
+            _taxCalcularotMock.Verify(f => f.CalculateTax(taxableIncome), Times.Once());
+            Assert.That(taxDeduction, Is.EqualTo(100)); 
         }
     }
 }
