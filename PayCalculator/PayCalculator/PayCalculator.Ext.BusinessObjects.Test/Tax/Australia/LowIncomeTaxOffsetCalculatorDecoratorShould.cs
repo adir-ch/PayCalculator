0092bcb7bad51256aeff485fc32d011662347506
adir-ch <actiger@gmail.com>
2017-02-27 15:52:13 +1100

    Created an extension point for the tax calculator.

---
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects.Test/Tax/Australia/LowIncomeTaxOffsetCalculatorDecoratorShould.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.Ext.BusinessObjects.Test.Tax.Australia
+{
+    [TestFixture]
+    public class LowIncomeTaxOffsetCalculatorDecoratorShould
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
---
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects/Tax/Australia/LowIncomeTaxOffsetCalculatorDecorator.cs
@@ -0,0 +1,17 @@
+﻿using PayCalculator.core.Model.Tax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.Ext.BusinessObjects.Tax.Australia
+{
+    public class LowIncomeTaxOffsetCalculatorDecorator : ITaxCalculatorDecorator
+    {
+        public decimal CalculateTax(decimal taxableIncome)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
