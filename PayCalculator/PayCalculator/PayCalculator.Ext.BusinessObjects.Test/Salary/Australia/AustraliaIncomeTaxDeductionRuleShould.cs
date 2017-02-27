2fdceee1024a57e3648ec90ca57c071713a8db89
adir-ch <actiger@gmail.com>
2017-02-27 12:23:21 +1100

    First test is failing - refactoring

---
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects.Test/Salary/Australia/AustraliaIncomeTaxDeductionRuleShould.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PayCalculator.Ext.BusinessObjects.Salary.Australia.DeductionRules; 
+
+namespace PayCalculator.Ext.BusinessObjects.Test.Salary.Australia
+{
+    [TestFixture]
+    public class AustraliaIncomeTaxDeductionRuleShould
+    {
+        private IncomeTaxDeductionRule _deductionRule;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _deductionRule = new IncomeTaxDeductionRule(); 
+        }
+
+        [Test]
+        public void SetAustralianTaxRuleDescription()
+        {
+            Assert.AreEqual(_deductionRule.GetRuleDescription(), "Income Tax"); 
+        }
+    }
+}
---
+++ PayCalculator/PayCalculator/PayCalculator.Ext.BusinessObjects/Salary/Australia/DeductionRules/IncomeTaxDeductionRule.cs
@@ -0,0 +1,24 @@
+﻿using PayCalculator.core.Model.Salary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator.Ext.BusinessObjects.Salary.Australia.DeductionRules
+{
+    public class IncomeTaxDeductionRule : IDeductionRule
+    {
+        public string RuleName { get; set; }
+        
+        public string GetRuleDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal Apply(decimal taxableIncome)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
